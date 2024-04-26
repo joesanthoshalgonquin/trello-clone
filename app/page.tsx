b8c65bc87a661783710f6788b13d9f5849bb8da5
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-04-26 13:22:25 -0400

    Added a header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
+import Header from "@/components/header.component";
+
 export default function Home() {
   return (
     <main>
-      <h1>Trello Clone</h1>
+      <Header />
     </main>
   );
 }
